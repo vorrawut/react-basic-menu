cd934809e9ffbfb04b5de520f00a64c8eb66b095
Vorrawut Judasri <vorrawutjud@gmail.com>
2018-03-28 21:01:32 +0700

    redesign of index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
-import React, { Component } from 'react';
+
+// React libs
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends Component {
-    render() {
-      return (
-        <div className="App">
-          <header className="App-header">
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
-      );
-    }
-}
+import  App from "./app/app";
 
+// Renderer React .
 ReactDOM.render(<App />, document.getElementById('root'));
 
 
