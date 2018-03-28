919d0fe694541ac0831dafe24278ec7e9b4ffa25
Vorrawut Judasri <vorrawutjud@gmail.com>
2018-03-28 20:56:31 +0700

    remove generate file and edit index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+class App extends Component {
+    render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      );
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+
+
